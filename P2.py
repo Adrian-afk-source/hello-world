385180ce6883f71ca52df9d719ddb3ddc64d3a25
Adrian-afk-source <madab1234567@gmail.com>
2024-10-10 21:50:11 -0500

    Add files via upload

---
+++ P2.py
@@ -0,0 +1,21 @@
+#Adrian Garcia
+#10/8/2024
+
+#The program greets the user.
+
+
+
+#Asks for the user for their name
+name = input("Please enter your name: ")
+name1 = "Adrian Garcia"
+name2 = "Benjamin A" 
+
+#Check if the user input is the same as name1
+#Greeting the user
+print(f"Hello, {name1}!") 
+print(f"Hello, {name2}!")
+
+#Program that will ask for userrs name and check age
+num = "22"
+int (num)
+int (num) /22
