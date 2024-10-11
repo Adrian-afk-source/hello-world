a8339d29ae38b0ad038c4d1324f024905046d122
Adrian-afk-source <madab1234567@gmail.com>
2024-10-10 21:46:40 -0500

    Add files via upload

---
+++ P4.py
@@ -0,0 +1,8 @@
+M = int(input("Enter Miles"))
+G = int(input("Enter Gallons"))
+C = M/G 
+print("The MPG is: ",C)
+
+#Adrian Garcia
+#10/10/2024
+#This program divides the Miles per gallon
---
+++ P5.py
@@ -0,0 +1,5 @@
+F = int(input("Enter the degrees Fahrenheit"))
+I = (F-32)*5/9
+if F<30:
+    print("That’s cold! In Norway you would say it is ",I," degrees C")
+
